bae05849732f65f122a4cc8b1937ef6884cf36ee
halimasoulb <47951591+halimasoulb@users.noreply.github.com>
2020-06-03 00:12:12 +0100

    Update request-cheque.service.ts

--- src/app/services/request-cheque.service.ts
+++ src/app/services/request-cheque.service.ts
@@ -11,20 +11,20 @@ export class RequestChequeService {
   constructor(private httpClient:HttpClient) { }
 
   public passercarte(vari,data){
-    return this.httpClient.post("$(environment.apiUrl}/requestCC/saveWithAccount/"+vari,data)
+    return this.httpClient.post("https://app-sad9a.herokuapp.com/requestCC/saveWithAccount/"+vari,data)
   }
 
   public annulercarte(vari){
-    return this.httpClient.delete("$(environment.apiUrl}/requestCC/"+vari+"/deleteRequest")
+    return this.httpClient.delete("https://app-sad9a.herokuapp.com/requestCC/"+vari+"/deleteRequest")
   }
   public getcarte(vari){
-    return this.httpClient.get("$(environment.apiUrl}/requestCC/"+vari)
+    return this.httpClient.get("https://app-sad9a.herokuapp.com/requestCC/"+vari)
   }
   public updatecarte(vari,data){
-    return this.httpClient.put("$(environment.apiUrl}/requestCC/update/"+vari,data)
+    return this.httpClient.put("https://app-sad9a.herokuapp.com/requestCC/update/"+vari,data)
   }
   public voircartes(vari){
-    return this.httpClient.get<requestcc[]>("$(environment.apiUrl}/requestCC/ByClient/"+vari)
+    return this.httpClient.get<requestcc[]>("https://app-sad9a.herokuapp.com/requestCC/ByClient/"+vari)
   }
 
   public getCompteCheckByClient(vari){
