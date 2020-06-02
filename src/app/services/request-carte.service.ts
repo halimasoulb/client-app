dd54e60a38eba49da30732cae415554b127d1c65
halimasoulb <47951591+halimasoulb@users.noreply.github.com>
2020-06-03 00:10:43 +0100

    Update request-carte.service.ts

--- src/app/services/request-carte.service.ts
+++ src/app/services/request-carte.service.ts
@@ -10,20 +10,20 @@ export class RequestCarteService {
   constructor(private httpClient:HttpClient) { }
 
   public passercarte(vari,data){
-    return this.httpClient.post("$(environment.apiUrl}/requestCartCredit/saveWithAccount/"+vari,data)
+    return this.httpClient.post("https://app-sad9a.herokuapp.com/requestCartCredit/saveWithAccount/"+vari,data)
   }
 
   public annulercarte(vari){
-    return this.httpClient.delete("$(environment.apiUrl}/requestCartCredit/"+vari+"/deleteRequest")
+    return this.httpClient.delete("https://app-sad9a.herokuapp.com/requestCartCredit/"+vari+"/deleteRequest")
   }
   public getcarte(vari){
-    return this.httpClient.get("$(environment.apiUrl}/requestCartCredit/"+vari)
+    return this.httpClient.get("https://app-sad9a.herokuapp.com/requestCartCredit/"+vari)
   }
   public updatecarte(vari,data){
-    return this.httpClient.put("$(environment.apiUrl}/requestCartCredit/terminer/"+vari,data)
+    return this.httpClient.put("https://app-sad9a.herokuapp.com/requestCartCredit/terminer/"+vari,data)
   }
   // a revoir
   public voircartes(vari){
-    return this.httpClient.get<Requestcb[]>("$(environment.apiUrl}/requestCartCredit/ByCompte/"+vari)
+    return this.httpClient.get<Requestcb[]>("https://app-sad9a.herokuapp.com/requestCartCredit/ByCompte/"+vari)
   }
 }
